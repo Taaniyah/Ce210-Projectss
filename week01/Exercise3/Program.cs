70dadc1a75164a304a01103ee079f42272167c3b
Taaniyah <122187661+Taaniyah@users.noreply.github.com>
2026-01-14 11:48:12 +0200

    Implement magic number guessing game
    Added functionality to guess a magic number.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -6,4 +6,32 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
     }
+    using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ask the user for the magic number
+        Console.Write("Enter the magic number: ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        // Ask the user for a guess
+        Console.Write("Enter your guess: ");
+        int guess = int.Parse(Console.ReadLine());
+
+        // Determine if the guess is higher, lower, or correct
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Guess higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Guess lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+    }
 }
